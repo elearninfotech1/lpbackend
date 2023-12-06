fbd90ec74712929b3dfdd70ef4e61f503bf48edc
elearninfotech1 <91933957+elearninfotech1@users.noreply.github.com>
2023-12-06 18:28:38 +0530

    first

---
+++ db.js
@@ -0,0 +1,4 @@
+let mongoose = require("mongoose");
+let url =
+  "mongodb+srv://elearninfotech1:th8L6eXnUMfgZPst@projectdb.7yydnsm.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(url);
